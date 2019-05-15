28e3b121fdec36e947ea6121b0b96f0c4b75fc8b
zoohan955 <ichekrigin@mail.ru>
2019-05-15 18:57:44 +1200

    templates

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+import datetime
+from django.utils import timezone
 # Create your models here.
 
 
--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,8 +1,15 @@
 
-from django.urls import path
+from django.urls import path,re_path
+
+from django.conf.urls import url
+
 
 from . import views
 
 urlpatterns = [
     path('',views.index, name='index'),
+    re_path(r'^(?P<question_id>[0-9]+)/$',views.detail,name='detail'),
+    re_path(r'^(?P<question_id>[0-9]+)/results/$',views.results,name='results'),
+    re_path(r'^(?P<question_id>[0-9]+)/vote/$',views.vote,name='vote',)
+
 ]
